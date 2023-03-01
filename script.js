2c52dd454b2c04e39f18b5bad871aff9f2405389
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2023-03-01 07:25:33 -0800

    last commit

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+// import 'core-js/stable';
+
+// import 'regenerator-runtime/runtime';
 const imgContainer = document.getElementById('img-container');
 const loader = document.getElementById('loader');
 
-const count = 30;
+let count = 5;
+
+let initialLoad = true;
+
 const apiKey = '0G6kQeQMBpX79Wlr8F-f-OnQzVhxb5DA0pHpIs2Qblo';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+const initalLoadcount = function (picCount) {
+  apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${picCount}`;
+};
 
 let photos = [];
+
 let loadImg = 0;
 let ready = false;
 let totalImages = 0;
@@ -20,13 +29,14 @@ const addImages = function () {
     loader.hidden = true;
   }
 };
+
 const setAttribute = function (el, attr) {
   for (const key in attr) {
     el.setAttribute(key, attr[key]);
   }
 };
 
-const getImages = function () {
+const getPhotos = function () {
   loadImg = 0;
   totalImages = photos.length;
   photos.forEach(photo => {
@@ -41,8 +51,8 @@ const getImages = function () {
 
     setAttribute(img, {
       src: photo.urls.regular,
+      description: photo.alt_description,
       alt: photo.alt_description,
-      title: photo.alt_description,
     });
 
     img.addEventListener('load', addImages);
@@ -55,10 +65,14 @@ const getImages = function () {
 const loadImages = async function () {
   try {
     const req = await fetch(apiUrl);
-
     photos = await req.json();
 
-    getImages();
+    getPhotos();
+
+    if (initialLoad) {
+      initalLoadcount(30);
+    }
+    initialLoad = false;
   } catch (err) {
     console.log(err);
   }
@@ -69,8 +83,8 @@ window.addEventListener('scroll', () => {
     window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
     ready
   ) {
+    console.log('scroll');
     ready = false;
-
     loadImages();
   }
 });
